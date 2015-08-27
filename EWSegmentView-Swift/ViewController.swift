1149801a4439d243dfe78f4949085ccfefef7092
wansy <wansy23@163.com>
2015-08-27 16:46:45 +0800

    Initial Commit

---
+++ EWSegmentView-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EWSegmentView-Swift
+//
+//  Created by wansy on 15/8/27.
+//  Copyright (c) 2015年 com.hengtiansoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
